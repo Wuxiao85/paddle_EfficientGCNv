a275ed039f2804626db04c0c93ef531af1afc6a5
Wuxiao85 <36037443+Wuxiao85@users.noreply.github.com>
2022-06-06 18:30:32 +0800

    Update tiny_data_gen.py

--- dataset/tiny_data_gen.py
+++ dataset/tiny_data_gen.py
@@ -13,14 +13,9 @@ def get_args(add_help=True):
     parser = argparse.ArgumentParser(
         description="gen sample data", add_help=add_help)
     parser.add_argument(
-        '--data_path',
+        '--source_path',
         type=str,
-        default='./data/npy_dataset/original/ntu-xsub/eval_data.npy',
-        help='save path of result data')
-    parser.add_argument(
-        '--label_path',
-        type=str,
-        default='./data/npy_dataset/original/ntu-xsub/eval_label.pkl',
+        default='./data/npy_dataset',
         help='save path of result data')
     parser.add_argument(
         '--save_dir',
@@ -32,35 +27,101 @@ def get_args(add_help=True):
         type=int,
         default=16*5,
         help='data num of result data')
+    parser.add_argument(
+        '--transform',
+        default=False, action='store_true') 
+    parser.add_argument(
+        '--dataset',
+        type=str,
+        default='ntu-xsub',
+    )
+    parser.add_argument(
+        '--mode',
+        type=str,
+        default='train',
+        help='train/eval')        
     args = parser.parse_args()
     return args
 
-def gen_tiny_data(data_path, label_path, save_dir, data_num, use_mmap=True):
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    if use_mmap:
-        data = np.load(data_path, mmap_mode='r')
+def gen_tiny_data(source_path, save_dir, data_num, use_mmap=True, mode='train'):
+    if mode == 'train':
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        train_data_path = os.path.join(source_path, 'train_data.npy')
+        train_data_label = os.path.join(source_path, 'train_label.pkl')
+        eval_data_path = os.path.join(source_path, 'eval_data.npy')
+        eval_data_label = os.path.join(source_path, 'eval_label.pkl')
+        if use_mmap:
+            train_data = np.load(train_data_path, mmap_mode='r')
+            eval_data = np.load(eval_data_path, mmap_mode='r')
+        else:
+            train_data = np.load(train_data_path)
+            eval_data = np.load(eval_data_path)
+        try:
+            with open(train_data_label) as f:
+                train_sample_name, train_label, train_seqlen = pickle.load(f)
+            with open(eval_data_label) as f:
+                eval_sample_name, eval_label, eval_seqlen = pickle.load(f)
+        except:
+            # for pickle file from python2
+            with open(train_data_label, 'rb') as f:
+                train_sample_name, train_label, train_seqlen = pickle.load(f, encoding='latin1')
+            with open(eval_data_label, 'rb') as f:
+                eval_sample_name, eval_label, eval_seqlen = pickle.load(f, encoding='latin1')
+            """with open(label_path, 'rb') as f:
+                sample_name, label, seqlen = pickle.load(f, encoding='latin1')"""
+        start = random.randint(0, len(train_label) - data_num)
+        train_label = train_label[start: start + data_num]
+        train_data = train_data[start: start + data_num]
+        train_seqlen = train_seqlen[start: start + data_num]
+        train_sample_name = train_sample_name[start: start + data_num]
+        with open(os.path.join(save_dir, f"train_label.pkl"), 'wb') as f:  
+            pickle.dump((train_sample_name, list(train_label),train_seqlen), f)
+        np.save(os.path.join(save_dir, f"train_data"), train_data)
+        start = random.randint(0, len(eval_label) - data_num)
+        eval_label = eval_label[start: start + data_num]
+        eval_data = eval_data[start: start + data_num]
+        eval_seqlen = eval_seqlen[start: start + data_num]
+        eval_sample_name = eval_sample_name[start: start + data_num]
+        with open(os.path.join(save_dir, f"eval_label.pkl"), 'wb') as f:  
+            pickle.dump((eval_sample_name, list(eval_label),eval_seqlen), f)
+        np.save(os.path.join(save_dir, f"eval_data"), eval_data)
+        print("Successfully generate tiny dataset")
+    elif mode == 'eval':
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        eval_data_path = os.path.join(source_path, 'eval_data.npy')
+        eval_data_label = os.path.join(source_path, 'eval_label.pkl')
+        if use_mmap:
+            eval_data = np.load(eval_data_path, mmap_mode='r')
+        else:
+            eval_data = np.load(eval_data_path)
+        try:
+            with open(eval_data_label) as f:
+                eval_sample_name, eval_label, eval_seqlen = pickle.load(f)
+        except:
+            # for pickle file from python2
+            with open(eval_data_label, 'rb') as f:
+                eval_sample_name, eval_label, eval_seqlen = pickle.load(f, encoding='latin1')
+            """with open(label_path, 'rb') as f:
+                sample_name, label, seqlen = pickle.load(f, encoding='latin1')"""
+        start = random.randint(0, len(eval_label) - data_num)
+
+        eval_label = eval_label[start: start + data_num]
+        eval_data = eval_data[start: start + data_num]
+        eval_seqlen = eval_seqlen[start: start + data_num]
+        eval_sample_name = eval_sample_name[start: start + data_num]
+        with open(os.path.join(save_dir, f"eval_label.pkl"), 'wb') as f:  
+            pickle.dump((eval_sample_name, list(eval_label),eval_seqlen), f)
+        np.save(os.path.join(save_dir, f"eval_data"), eval_data)
+        print("Successfully generate tiny dataset")
     else:
-        data = np.load(data_path)
-    try:
-        with open(label_path) as f:
-            sample_name, label, seqlen = pickle.load(f)
-    except:
-        # for pickle file from python2
-        with open(label_path, 'rb') as f:
-            sample_name, label, seqlen = pickle.load(f, encoding='latin1')
-    start = random.randint(0, len(label) - data_num)
-    label = label[start: start + data_num]
-    data = data[start: start + data_num]
-    seqlen = seqlen[start: start + data_num]
-    sample_name = sample_name[start: start + data_num]
+        raise NotImplementedError
     
-    with open(os.path.join(save_dir, "tiny_infer_label.pkl"), 'wb') as f:  
-        pickle.dump((sample_name, list(label),seqlen), f)
-    np.save(os.path.join(save_dir, "tiny_infer_data"), data)
-    print("Successfully generate tiny dataset")
 
 
 if __name__ == "__main__":
     args = get_args()
-    gen_tiny_data(args.data_path, args.label_path, args.save_dir, data_num=args.data_num)
+    gen_tiny_data(os.path.join(args.source_path, 'transform' if args.transform else 'original', args.dataset), 
+            os.path.join(args.save_dir, 'transform' if args.transform else 'original', args.dataset), 
+            data_num=args.data_num, mode=args.mode)
